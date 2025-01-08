d6076f83543799bd76b31124dfce53b9a810e155
Marcelus <marcelusebrown@gmail.com>
2025-01-08 14:02:44 -0500

    Update assets.js

---
+++ Assets/assets.js
@@ -0,0 +1,28 @@
+import diploma_pic from './Diploma.JPG'
+import flocked_pic from './Flocked.JPG'
+import graduates_by_trees_pic from './GraduatesByTrees.JPG'
+import graduation_pic from './Graduation.JPG'
+import kb_graduation_pic from './KNBGraduationCap.JPG'
+import project_grad_party_pic from 'ProjectGradParty.JPG'
+import project_graduation_banner_pic from './ProjectGraduationBanner.JPG'
+import projectgrad_logo from './WHS_Project_Graduation_Logo.png'
+import wps_logo from './WindsorPublicSchoolsLogo.svg'
+import windsor_w_logo from './WindsorW.png'
+
+export const assets = {
+    diploma_pic,
+    flocked_pic,
+    graduates_by_trees_pic,
+    graduation_pic,
+    kb_graduation_pic,
+    project_grad_party_pic,
+    project_graduation_banner_pic,
+    projectgrad_logo,
+    wps_logo,
+    windsor_w_logo
+}
+
+export const blog_data = [{
+
+},
+]
