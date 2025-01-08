53747339d5c1a3f89dbae61c32515ed54c70e356
Marcelus <marcelusebrown@gmail.com>
2025-01-08 14:48:34 -0500

    fix formatting
    everything shows correctly now

--- Components/BlogItem.jsx
+++ Components/BlogItem.jsx
@@ -13,14 +13,14 @@ const BlogItem = () => {
         height={400}
         className='border-b border-black'
       />
-      <p className='ml-5 mt-5 px-1 inline-block bg-black text-black text-sm'>
+      <p className='ml-5 mt-5 px-1 inline-block bg-black text-white text-sm'>
         {blog_data[0].category}
       </p>
       <div className='p-5'>
         <h5 className='mb-2 text-lg font-medium tracking-tight text-black'>
           {blog_data[0].title}
         </h5>
-        <p className='mb-3 text-sm tracking-tight bg-black text-black'>
+        <p className='mb-3 text-sm tracking-tight text-black'>
           {blog_data[0].description}
         </p>
         <div className='text-black inline-flex items-center py-2 font-semibold text-center'>
