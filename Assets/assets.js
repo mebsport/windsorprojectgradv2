1d418b617b32500551b802552cbc87f24064a3fc
Marcelus <marcelusebrown@gmail.com>
2025-01-08 14:15:34 -0500

    Update assets.js

--- Assets/assets.js
+++ Assets/assets.js
@@ -1,28 +1,10 @@
-import diploma_pic from './Diploma.JPG'
-import flocked_pic from './Flocked.JPG'
-import graduates_by_trees_pic from './GraduatesByTrees.JPG'
-import graduation_pic from './Graduation.JPG'
-import kb_graduation_pic from './KNBGraduationCap.JPG'
-import project_grad_party_pic from 'ProjectGradParty.JPG'
-import project_graduation_banner_pic from './ProjectGraduationBanner.JPG'
-import projectgrad_logo from './WHS_Project_Graduation_Logo.png'
-import wps_logo from './WindsorPublicSchoolsLogo.svg'
-import windsor_w_logo from './WindsorW.png'
-
-export const assets = {
-    diploma_pic,
-    flocked_pic,
-    graduates_by_trees_pic,
-    graduation_pic,
-    kb_graduation_pic,
-    project_grad_party_pic,
-    project_graduation_banner_pic,
-    projectgrad_logo,
-    wps_logo,
-    windsor_w_logo
-}
-
 export const blog_data = [{
+    id: 1,
+    title: 'example post',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    image: '/Images/WHS_Project_Graduation_Logo.png',    
+    date: Date.now(),
+    category: "misc"
 
 },
 ]
