95601598b66c187962e1546e75ec179c8b42ea38
Marcelus <marcelusebrown@gmail.com>
2025-01-08 02:53:20 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
-import NavBar from "@/Components/NavBar";
+import HeroSection from "@/Components/HeroSection";
+import HeroSection from "@/Components/NavBar";
 
 export default function Home() {
   return (
-    <>
-    <NavBar></NavBar>
-    </>
+    <main className="overflow-x-hidden flex min-h-screen flex-col bg-white">
+    <NavBar/>
+    <HeroSection/>
+    </main>
   );
 }
