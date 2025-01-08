fa634ed0bb26da97f22958aa523b1b1e223c0a85
mebsport <56307645+mebsport@users.noreply.github.com>
2025-01-08 02:43:10 -0500

    Create MenuOverlay.jsx

---
+++ Components/MenuOverlay.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MobileNavLink from './MobileNavLink'
+
+const MenuOverlay = ({ links }) => {
+  return (
+    <ul className='flex md:hidden flex-col py-4 items-center'>
+      {links.map((link, index) => (
+        <li key={index}>
+          <MobileNavLink href={link.href} title={link.title} />
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default MenuOverlay
