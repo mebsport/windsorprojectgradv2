6faf4ddde42ba1a5cef3fc91f777c7fbdf7b046a
Marcelus <marcelusebrown@gmail.com>
2025-01-08 17:37:19 -0500

    Update HeroSection.jsx

--- Components/HeroSection.jsx
+++ Components/HeroSection.jsx
@@ -1,7 +1,7 @@
-'use client'
-import React from 'react'
-import Image from 'next/image'
-import { TypeAnimation } from 'react-type-animation'
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
@@ -10,22 +10,22 @@ const HeroSection = () => {
         <div className='col-span-7 place-self-center text-center sm:text-left'>
           <h1 className='text-black mb-4 text-xl lg:text-2xl font-extrabold'>
             <span className='text-3xl lg:text-5xl text-transparent bg-clip-text bg-gradient-to-r from-[#8c2724] via-black to-[#8c2724]'>
-              Windsor Project Graduation {''}
+              Windsor Project Graduation {""}
             </span>
             <br></br>
             <TypeAnimation
               style={{
-                display: 'inline-block',
-                minHeight: '3lh',
+                display: "inline-block",
+                minHeight: "3lh",
               }}
               sequence={[
-                'We emphasize making the choice not to drink alcohol or consume drugs when celebrating a special milestone',
+                "We emphasize making the choice not to drink alcohol or consume drugs when celebrating a special milestone",
                 2500,
-                'We eliminate teen decisions about drinking or drugs on graduation night, and the entire community is assured that graduates are in a safe environment',
+                "We eliminate teen decisions about drinking or drugs on graduation night, and the entire community is assured that graduates are in a safe environment",
                 2500,
-                'We ensure that every graduate has a party to attend; no one is excluded or left out for social or financial reasons',
+                "We ensure that every graduate has a party to attend; no one is excluded or left out for social or financial reasons",
                 2500,
-                'We allow all graduates to celebrate together in one location',
+                "We allow all graduates to celebrate together in one location",
                 2500,
               ]}
               wrapper='span'
@@ -36,7 +36,7 @@ const HeroSection = () => {
           <p className='text-black text-base sm:text-lg lg:text-xl mb-6'>
             Windsor High School&apos;s Project Graduation gives our graduates a
             safe, sober and memorable night of Graduation. The mission of
-            PROJECT GRADUATION is to protect our new Graduates at the time
+            Project Graduation is to protect our new Graduates at the time
             they&apos;re most at risk. Since Project Graduation was instituted,
             not a single Windsor graduate has died from alcohol, drug or traffic
             accidents on graduation night.
@@ -44,20 +44,20 @@ const HeroSection = () => {
           <div className='place-self-center'>
             <button
               onClick={() =>
-                window.open('https://cash.app/$ProjectGraduationWHS', '_blank')
+                window.open("https://cash.app/$ProjectGraduationWHS", "_blank")
               }
               className=' font-mono font-bold px-6 py-3 rounded-full ml-4 mr-4 bg-[#131313] hover:bg-slate-200 text-white border border-white hover:text-black mt-3'
             >
               DONATE
             </button>
             <button
-              onClick={() => window.open('/volunteer', '_self')}
+              onClick={() => window.open("/volunteer", "_self")}
               className=' font-mono font-bold px-6 py-3 rounded-full ml-4 mr-4 bg-[#131313] hover:bg-slate-200 text-white border border-white hover:text-black mt-3'
             >
               VOLUNTEER
             </button>
             <button
-              onClick={() => window.open('#about', '_self')}
+              onClick={() => window.open("#about", "_self")}
               className=' font-mono font-bold px-6 py-3 rounded-full ml-4 mr-4 bg-[#131313] hover:bg-slate-200 text-white border border-white hover:text-black mt-3'
             >
               LEARN MORE
@@ -75,7 +75,7 @@ const HeroSection = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
