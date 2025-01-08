9deab279766454d463ba1e90ee5f022febf9d39f
Marcelus <marcelusebrown@gmail.com>
2025-01-08 02:58:35 -0500

    Adjust Formatting
    Adjust the formatting and margins on the main page so that the HeroSection is not hidden behind the navbar

--- app/page.js
+++ app/page.js
@@ -5,7 +5,9 @@ export default function Home() {
   return (
     <main className="overflow-x-hidden flex min-h-screen flex-col bg-white">
     <NavBar/>
+    <div className="container mt-24 mx-auto py-4 px-12">
     <HeroSection/>
+    </div>
     </main>
   );
 }
