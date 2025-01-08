25c31ea359fccce2acb39b24d26414c7c8c3a4ae
Marcelus <marcelusebrown@gmail.com>
2025-01-08 14:38:30 -0500

    Update BlogItem.jsx
    needs to be fixed later, formatting does not look right

--- Components/BlogItem.jsx
+++ Components/BlogItem.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
+import { blog_data } from "@/Assets/assets";
+import { ArrowRightIcon } from "@heroicons/react/24/solid";
 
 const BlogItem = () => {
   return (
-    <div className='max-w-[330px] sm:max-w-[300px] bg-2hite border border-black hover:shadow-[-7px_7px_0px_#000000]'>
-        {/* <Image src=' '/> */}
+    <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]'>
+      <Image
+        src={blog_data[0].image}
+        alt=''
+        width={400}
+        height={400}
+        className='border-b border-black'
+      />
+      <p className='ml-5 mt-5 px-1 inline-block bg-black text-black text-sm'>
+        {blog_data[0].category}
+      </p>
+      <div className='p-5'>
+        <h5 className='mb-2 text-lg font-medium tracking-tight text-black'>
+          {blog_data[0].title}
+        </h5>
+        <p className='mb-3 text-sm tracking-tight bg-black text-black'>
+          {blog_data[0].description}
+        </p>
+        <div className='text-black inline-flex items-center py-2 font-semibold text-center'>
+          <h5>Read More</h5> <ArrowRightIcon width={16} className='ml-2' />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BlogItem
+export default BlogItem;
