58c2251d5ac8a95caf22e957e8f339668ab5c3d9
mebsport <56307645+mebsport@users.noreply.github.com>
2025-01-08 12:04:09 -0500

    Formatting

--- app/layout.js
+++ app/layout.js
@@ -1,24 +1,21 @@
-import { Outfit} from "next/font/google";
-import "./globals.css";
+import { Outfit } from 'next/font/google'
+import './globals.css'
 
 const outfit = Outfit({
-  subsets: ["latin"],weight:["400","500","600","700"]
-});
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+})
 
 export const metadata = {
-  title: "Windsor Project Graduation",
-  description:     "Windsor High School's Project Graduation for the class of 2024 provides a fun and safe enviroments for graudates to celebrate after the big day.",
-
-};
+  title: 'Windsor Project Graduation',
+  description:
+    "Windsor High School's Project Graduation for the class of 2024 provides a fun and safe enviroments for graudates to celebrate after the big day.",
+}
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${outfit.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang='en'>
+      <body className={`${outfit.variable} antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
--- app/page.js
+++ app/page.js
@@ -1,13 +1,13 @@
-import HeroSection from "@/Components/HeroSection";
-import NavBar from "@/Components/NavBar";
+import HeroSection from '@/Components/HeroSection'
+import NavBar from '@/Components/NavBar'
 
 export default function Home() {
   return (
-    <main className="overflow-x-hidden flex min-h-screen flex-col bg-white">
-    <NavBar/>
-    <div className="container mt-24 mx-auto py-4 px-12">
-    <HeroSection/>
-    </div>
+    <main className='overflow-x-hidden flex min-h-screen flex-col bg-white'>
+      <NavBar />
+      <div className='container mt-24 mx-auto py-4 px-12'>
+        <HeroSection />
+      </div>
     </main>
-  );
+  )
 }
