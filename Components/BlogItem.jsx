e198fb2e815b03460c541f606313c1ddc7c72d06
mebsport <56307645+mebsport@users.noreply.github.com>
2025-01-08 13:43:22 -0500

    Update BlogItem.jsx

--- Components/BlogItem.jsx
+++ Components/BlogItem.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Image from 'next/image'
 
 const BlogItem = () => {
   return (
-    <div>BlogItem</div>
+    <div className='max-w-[330px] sm:max-w-[300px] bg-2hite border border-black hover:shadow-[-7px_7px_0px_#000000]'>
+        <Image src=' '/>
+    </div>
   )
 }
 
