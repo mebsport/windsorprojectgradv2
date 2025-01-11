e771b746af0d190a2bd0fef664799177c5a5fde4
Marcelus <marcelusebrown@gmail.com>
2025-01-10 23:55:05 -0500

    Update AboutSection.jsx

--- Components/AboutSection.jsx
+++ Components/AboutSection.jsx
@@ -1,42 +1,44 @@
-import React from 'react'
-import ImageCarousel from './ImageCarousel'
+import React from "react";
+import ImageCarousel from "./ImageCarousel";
 import { image_urls } from "@/Assets/assets";
 
 const AboutSection = () => {
   return (
-    <div id='about' className='mt-4'>
-      <span className='mt-4 font-bold text-2xl lg:text-4xl text-transparent bg-clip-text bg-gradient-to-r from-[#8c2724] via-black to-[#8c2724]'>
-        What is Project Graduation? {''}
+    <div id='about' className='mt-4 flex flex-col'>
+      <span className='text-center mt-4 font-bold text-2xl lg:text-4xl text-transparent bg-clip-text bg-gradient-to-r from-[#8c2724] via-black to-[#8c2724]'>
+        What is Project Graduation? {""}
       </span>
-      <div>
-        <p className='mt-6 text-black font-semibold text-base sm:text-lg lg:text-xl mb-6'>
-          Project Graduation is an annual event that is exclusively organized
-          for the graduating seniors of Windsor High School. The event is aimed
-          at providing a safe, drug-free, and fun-filled environment for the
-          seniors to celebrate their graduation with their friends. The event is
-          entirely managed and organized by parent volunteers, who put in a
-          year-long effort to raise funds and plan for the event.
-        </p>
-        <p className='text-black font-semibold text-base sm:text-lg lg:text-xl mb-6'>
-          The night is full of entertaining activities, including music and
-          dancing with a live DJ, delicious food, fun games, and exciting
-          prizes. The main objective of Project Graduation is to offer the
-          seniors a chance to celebrate their graduation in a secure and
-          comfortable environment, away from the potential dangers of drugs and
-          alcohol.
-        </p>
-        <p className='text-black font-semibold text-base sm:text-lg lg:text-xl mb-6'>
-          This event is made possible through the generous donations of the
-          community members who believe in the importance of providing a safe
-          and memorable graduation experience for the seniors. The party starts
-          at 11 PM and ends at 3 AM, and all the seniors are guaranteed to have
-          a great time, create everlasting memories, and stay safe throughout
-          the night.
-        </p>
+      <div className='grid grid-cols-3'>
+        <div className='grid-column: span 2'>
+          <p className='mt-6 text-black font-semibold text-base sm:text-lg lg:text-xl mb-6'>
+            Project Graduation is an annual event that is exclusively organized
+            for the graduating seniors of Windsor High School. The event is
+            aimed at providing a safe, drug-free, and fun-filled environment for
+            the seniors to celebrate their graduation with their friends. The
+            event is entirely managed and organized by parent volunteers, who
+            put in a year-long effort to raise funds and plan for the event.
+          </p>
+          <p className='text-black font-semibold text-base sm:text-lg lg:text-xl mb-6'>
+            The night is full of entertaining activities, including music and
+            dancing with a live DJ, delicious food, fun games, and exciting
+            prizes. The main objective of Project Graduation is to offer the
+            seniors a chance to celebrate their graduation in a secure and
+            comfortable environment, away from the potential dangers of drugs
+            and alcohol.
+          </p>
+          <p className='text-black font-semibold text-base sm:text-lg lg:text-xl mb-6'>
+            This event is made possible through the generous donations of the
+            community members who believe in the importance of providing a safe
+            and memorable graduation experience for the seniors. The party
+            starts at 11 PM and ends at 3 AM, and all the seniors are guaranteed
+            to have a great time, create everlasting memories, and stay safe
+            throughout the night.
+          </p>
+        </div>
+        <ImageCarousel images={image_urls} interval={5000} />
       </div>
-      <ImageCarousel images={image_urls} interval={5000}/>
     </div>
-  )
-}
+  );
+};
 
-export default AboutSection
+export default AboutSection;
