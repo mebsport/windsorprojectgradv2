53608884fd73a0c482cd049f03070a25eba32209
Marcelus <marcelusebrown@gmail.com>
2025-01-10 23:41:46 -0500

    Update selection options

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -7,12 +7,13 @@ const BlogList = () => {
 
   return (
     <div>
-      <div className='flex justify-center gap-6 my-10 text-black'>
+      <div className='mb-2 flex justify-center gap-6 my-10 text-black'>
         <button className='bg-black text-white py-1 px-4 rounded-sm'>
           All
         </button>
-        <button className='px-2'>Fundraising</button>
-        <button className='px-2'>Event</button>
+        <button className='py-1 px-4'>Event</button>
+        <button className='py-1 px-4'>Meeting</button>
+        <button className='py-1 px-4'>Volunteer</button>
       </div>
       <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
         {blog_data.map((item, index) => {
